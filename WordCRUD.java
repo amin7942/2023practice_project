fd0b17864b8d78482d75c16ff45f3a0adfea0ef4
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-09-12 10:55:48 +0900

    수정 기능 추가

--- WordCRUD.java
+++ WordCRUD.java
@@ -17,7 +17,25 @@ public class WordCRUD {
         list.add(new Word(0, level, word, meaning));
         System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
-
+    public void update(){
+        System.out.println("==> 수정 할 단어 검색 : ");
+        String search = sc.nextLine();
+        System.out.println("----------------------------");
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).getWord().contains(search)){
+                System.out.print((i+1)+" ");
+                System.out.println(list.get(i).toString());
+            }
+        }
+        System.out.println("----------------------------");
+        System.out.println("==> 수정 할 번호 선택 : ");
+        int num = sc.nextInt() - 1;
+        sc.nextLine();
+        System.out.println("뜻 입력 : ");
+        String str = sc.nextLine();
+        list.get(num).setMeaning(str);
+        System.out.println("단어가 수정 되었습니다.");
+    }
     public void listAll(){
         System.out.println("----------------------------");
         for(int i = 0; i < list.size(); i++){
