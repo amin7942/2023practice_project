49595a7428720895b9fedb469c9d576b253b0426
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-09-12 11:10:58 +0900

    삭제기능 추가, 수정기능 수정

--- WordCRUD.java
+++ WordCRUD.java
@@ -17,9 +17,7 @@ public class WordCRUD {
         list.add(new Word(0, level, word, meaning));
         System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
-    public void update(){
-        System.out.println("==> 수정 할 단어 검색 : ");
-        String search = sc.nextLine();
+    public void search_list(String search){
         System.out.println("----------------------------");
         for(int i = 0; i < list.size(); i++){
             if(list.get(i).getWord().contains(search)){
@@ -28,6 +26,11 @@ public class WordCRUD {
             }
         }
         System.out.println("----------------------------");
+    }
+    public void update(){
+        System.out.println("==> 수정 할 단어 검색 : ");
+        String search = sc.nextLine();
+        search_list(search);
         System.out.println("==> 수정 할 번호 선택 : ");
         int num = sc.nextInt() - 1;
         sc.nextLine();
@@ -35,6 +38,22 @@ public class WordCRUD {
         String str = sc.nextLine();
         list.get(num).setMeaning(str);
         System.out.println("단어가 수정 되었습니다.");
+    }
+    public void remove(){
+        System.out.println("삭제할 단어 검색 : ");
+        String search = sc.nextLine();
+        search_list(search);
+        System.out.println("삭제할 번호 선택 : ");
+        int num = sc.nextInt() - 1;
+        System.out.println("==> 정말로 삭제하실래요?(Y/n)");
+        sc.nextLine();
+        String check = sc.next();
+        if(check.equalsIgnoreCase("y")){
+            list.remove(num);
+            System.out.println("단어가 삭제되었습니다.");
+        }else{
+            System.out.println("취소되었습니다.");
+        }
     }
     public void listAll(){
         System.out.println("----------------------------");
