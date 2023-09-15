53036eb1334af6d0436d60d41b2f5a7342eacfdd
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-09-15 11:03:43 +0900

    모든 단어 보기 단어 검색 수준별 단어 검색 단어 삭제 단어 수정 파일 저장 파일 읽기 기능 구현

--- WordCRUD.java
+++ WordCRUD.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -5,6 +6,8 @@ import java.util.Scanner;
 public class WordCRUD {
     private ArrayList <Word> list;
     Scanner sc = new Scanner(System.in);
+    String path = System.getProperty("user.dir") + "\\src\\";
+    final String fname = "Dictionary.txt";
     public WordCRUD() {
         list = new ArrayList<Word>();
     }
@@ -24,6 +27,25 @@ public class WordCRUD {
                 System.out.print((i+1)+" ");
                 System.out.println(list.get(i).toString());
             }
+        }
+        System.out.println("----------------------------");
+    }
+    public void search(){
+        System.out.println("검색할 단어 입력");
+        String str = sc.nextLine();
+        search_list(str);
+    }
+    public void step_search(){
+        System.out.println("원하는 레벨은?");
+        int level = sc.nextInt();
+        System.out.println("----------------------------");
+        int i = 0;
+        for(Word w : list){
+            if(w.getLevel() == level){
+                System.out.print((i+1)+" ");
+                System.out.println(list.get(i).toString());
+            }
+            i++;
         }
         System.out.println("----------------------------");
     }
@@ -64,4 +86,38 @@ public class WordCRUD {
         System.out.println("----------------------------");
     }
 
+    public void loadFile(){
+        try {
+            String line;
+            int count = 0;
+            BufferedReader br = new BufferedReader(new FileReader(path + fname));
+            while(true) {
+                line = br.readLine();
+                if (line == null) break;
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("==>" + count +"개 로딩 완료!!!");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void saveFile(){
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(path + "Dictionary.txt"));
+            for(Word one : list){
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장 완료!!");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
